f1570ab6e9b561d8b0c9492a5c6833f1a1bc0282
hozturk0327@gmail.com <hozturk0327@gmail.com>
2019-01-26 14:56:28 +0300

    Prepared the base code for selecting a tile

---
+++ Assets/Player/PlayerActions.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerActions : MonoBehaviour
+{
+    public Sprite t_water;
+    public Sprite t_lava;
+    public Sprite t_brick;
+    public Sprite t_ice;
+    public Sprite t_grass;
+    public Sprite t_stone;
+
+    public GameObject cube;
+
+    Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = FindObjectOfType<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float mouseX = Input.mousePosition.x;
+            float mouseY = Input.mousePosition.y;
+            Vector3 clickPoint = Camera.main.ScreenToWorldPoint(new Vector3(mouseX, mouseY, 10));
+
+            Vector3Int cellPos = tilemap.WorldToCell(clickPoint);
+            Sprite sprite = tilemap.GetSprite(cellPos);
+
+            if (sprite.name == t_water.name)
+            {
+
+            }
+            else if (sprite.name == t_lava.name)
+            {
+
+            }
+            else if (sprite.name == t_brick.name)
+            {
+
+            }
+            else if (sprite.name == t_ice.name)
+            {
+
+            }
+            else if (sprite.name == t_grass.name)
+            {
+
+            }
+            else if (sprite.name == t_stone.name)
+            {
+
+            }
+        }
+    }
+}
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float movementSpeedMultiplier = 8;
+    public float cameraFocusSpeedMultiplier = 6;
+    public int cameraFocusMin = 35, cameraFocusMax = 15;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float focus = Input.GetAxis("Focus");
+
+        transform.Translate(new Vector2(horizontal, vertical) * movementSpeedMultiplier * Time.deltaTime);
+
+        Camera.main.orthographicSize -= (focus * cameraFocusSpeedMultiplier * Time.deltaTime);
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, cameraFocusMax, cameraFocusMin);
     }
 }
