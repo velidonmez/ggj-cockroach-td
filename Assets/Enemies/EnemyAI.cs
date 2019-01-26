e1b54aad1aeacb447f35d278faa3cab731b28591
hozturk0327@gmail.com <hozturk0327@gmail.com>
2019-01-26 18:26:39 +0300

    Added enemy and waypoints for enemy to move between

---
+++ Assets/Enemies/EnemyAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float movementSpeed = 3;
+
+    Waypoints waypoints;
+    int target = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = FindObjectOfType<Waypoints>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target >= 0)
+        {
+            print(waypoints.waypoints[target].transform.position);
+            Vector3 distance = waypoints.waypoints[target].transform.position - transform.position;
+            Vector2 distance2D = new Vector2(distance.x, distance.y);
+            Vector3 movement = distance2D.normalized * movementSpeed * Time.deltaTime;
+            transform.position += movement;
+
+            if (distance2D.sqrMagnitude < 2)
+            {
+                target++;
+                if (target >= waypoints.waypoints.Length)
+                    target = -1;
+            }
+        }
+    }
+}
